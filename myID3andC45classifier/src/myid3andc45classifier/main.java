fdb0333c8e87e145e7d4b0f440e5a0958283b241
ryanyonata <ryan.yonata@gmail.com>
2016-10-04 15:08:09 +0700

    Project initiation

---
+++ myID3andC45classifier/src/myid3andc45classifier/main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myid3andc45classifier;
+
+import weka.core.Instances;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import weka.core.converters.ConverterUtils.DataSource;
+
+/**
+ *
+ * @author ryanyonata
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here 
+        
+    }
+    
+    public void readARFF(String filepath) throws Exception {
+        DataSource source = new DataSource("weather.nominal.arff");
+        Instances data = source.getDataSet();
+        // setting class attribute if the data format does not provide this information
+        // For example, the XRFF format saves the class attribute information as well
+        if (data.classIndex() == -1)
+          data.setClassIndex(data.numAttributes() - 1);
+    }
+    
+}
