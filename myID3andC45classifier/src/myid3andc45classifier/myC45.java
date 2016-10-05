a7d62ac8bff8df68a8e7462e209ef8b51c65ee28
bayuRP <13513046@std.stei.itb.ac.id>
2016-10-05 17:41:28 +0700

    initial c45

--- myID3andC45classifier/src/myid3andc45classifier/myC45.java
+++ myID3andC45classifier/src/myid3andc45classifier/myC45.java
@@ -5,10 +5,20 @@
  */
 package myid3andc45classifier;
 
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+
 /**
  *
  * @author ryanyonata
  */
-public class myC45 {
+public class myC45 extends Classifier {
+
+    @Override
+    public void buildClassifier(Instances data) throws Exception {
+        data = new Instances(data);
+        data.deleteWithMissingClass();
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
